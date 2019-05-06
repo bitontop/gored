834fde204bfa0181bbbd3d2a74f8acedadb4219d
Tony <604260645@qq.com>
2019-05-06 10:50:06 -0700

    init

---
+++ exchange/coinegg/meta.go
@@ -0,0 +1,13 @@
+package binance
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const (
+	DEFAULT_ID           = 1
+	DEFAULT_TAKER_FEE    = 0.001
+	DEFAULT_MAKERER_FEE  = 0.001
+	DEFAULT_LOT_SIZE     = 0.00000001
+	DEFAULT_PRICE_FILTER = 0.00000001 //PRICE FILTER
+)
