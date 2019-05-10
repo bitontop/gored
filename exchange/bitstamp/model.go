5eaacffca7d6a9ef36b5a5751d187e601683889c
Tony <604260645@qq.com>
2019-05-10 11:54:18 -0700

    fix bitstamp

--- exchange/bitstamp/model.go
+++ exchange/bitstamp/model.go
@@ -20,14 +20,6 @@ type CoinsData []struct {
 	WithdrawFee         string `json:"withdrawFee"`
 }
 
-type PairsDataaaa []struct {
-	ID            string `json:"id"`
-	BaseCurrency  string `json:"baseCurrency"`
-	QuoteCurrency string `json:"quoteCurrency"`
-	LotSize       string `json:"lotSize"`
-	TickSize      string `json:"tickSize"`
-}
-
 type PairsData []struct {
 	BaseDecimals    int    `json:"base_decimals"`
 	MinimumOrder    string `json:"minimum_order"`
@@ -43,74 +35,3 @@ type OrderBook struct {
 	Bids      [][]string `json:"bids"`
 	Asks      [][]string `json:"asks"`
 }
-
-/* type OrderBook struct {
-	Buy  []exchange.Order `json:"buy"`
-	Sell []exchange.Order `json:"sell"`
-} */
-
-/* type CoinsData []struct {
-	Currency        string      `json:"Currency"`
-	CurrencyLong    string      `json:"CurrencyLong"`
-	MinConfirmation int         `json:"MinConfirmation"`
-	TxFee           float64     `json:"TxFee"`
-	IsActive        bool        `json:"IsActive"`
-	IsRestricted    bool        `json:"IsRestricted"`
-	CoinType        string      `json:"CoinType"`
-	BaseAddress     string      `json:"BaseAddress"`
-	Notice          interface{} `json:"Notice"`
-} */
-
-/* type PairsData []struct {
-	MarketCurrency     string      `json:"MarketCurrency"`
-	BaseCurrency       string      `json:"BaseCurrency"`
-	MarketCurrencyLong string      `json:"MarketCurrencyLong"`
-	BaseCurrencyLong   string      `json:"BaseCurrencyLong"`
-	MinTradeSize       float64     `json:"MinTradeSize"`
-	MarketName         string      `json:"MarketName"`
-	IsActive           bool        `json:"IsActive"`
-	Created            string      `json:"Created"`
-	Notice             interface{} `json:"Notice"`
-	IsSponsored        interface{} `json:"IsSponsored"`
-	LogoURL            string      `json:"LogoUrl"`
-} */
-/*
-type AccountBalances []struct {
-	Currency      string  `json:"Currency"`
-	Balance       float64 `json:"Balance"`
-	Available     float64 `json:"Available"`
-	Pending       float64 `json:"Pending"`
-	CryptoAddress string  `json:"CryptoAddress"`
-	Requested     bool    `json:"Requested"`
-	Uuid          string  `json:"Uuid"`
-}
-
-type Uuid struct {
-	Id string `json:"uuid"`
-}
-
-type PlaceOrder struct {
-	AccountId                  string
-	OrderUuid                  string `json:"OrderUuid"`
-	Exchange                   string `json:"Exchange"`
-	Type                       string
-	Quantity                   float64 `json:"Quantity"`
-	QuantityRemaining          float64 `json:"QuantityRemaining"`
-	Limit                      float64 `json:"Limit"`
-	Reserved                   float64
-	ReserveRemaining           float64
-	CommissionReserved         float64
-	CommissionReserveRemaining float64
-	CommissionPaid             float64
-	Price                      float64 `json:"Price"`
-	PricePerUnit               float64 `json:"PricePerUnit"`
-	Opened                     string
-	Closed                     string
-	IsOpen                     bool
-	Sentinel                   string
-	CancelInitiated            bool
-	ImmediateOrCancel          bool
-	IsConditional              bool
-	Condition                  string
-	ConditionTarget            string
-} */
