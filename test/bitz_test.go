541b2fc3bc130a1e154a5f31c915a3d60cc15560
botchun <chunlee@bitontop.com>
2019-05-14 16:16:52 -0700

    format

--- test/bitz_test.go
+++ test/bitz_test.go
@@ -6,12 +6,9 @@ import (
 
 	"github.com/bitontop/gored/coin"
 	"github.com/bitontop/gored/exchange"
+	"github.com/bitontop/gored/exchange/bitz"
 	"github.com/bitontop/gored/pair"
-
-	//"github.com/bitontop/gored/exchange/bitz"
-	//"github.com/bitontop/gored/test/conf"
-	"../exchange/bitz"
-	"./conf"
+	"github.com/bitontop/gored/test/conf"
 )
 
 // Copyright (c) 2015-2019 Bitontop Technologies Inc.
