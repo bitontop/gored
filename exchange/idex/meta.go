92a008a74d6607ce57219d7bc79bf99de3519417
botchun <chunlee@bitontop.com>
2019-05-17 11:10:06 -0700

    add idex public

---
+++ exchange/idex/meta.go
@@ -0,0 +1,20 @@
+package idex
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const (
+	DEFAULT_ID           = 20
+	DEFAULT_TAKER_FEE    = 0.002
+	DEFAULT_MAKER_FEE    = 0.001
+	DEFAULT_LOT_SIZE     = 0.00000001
+	DEFAULT_PRICE_FILTER = 0.00000001 //PRICE FILTER
+	DEFAULT_TXFEE        = 0.005
+	DEFAULT_WITHDRAW     = true
+	DEFAULT_DEPOSIT      = true
+	DEFAULT_CONFIRMATION = 2
+
+	CONTRACT_ADDRESS = "0x2a0c0dbecc7e4d658f48e01e3fa353f44050c208"
+	EXPIRES          = 100000
+)
--- exchange/meta.go
+++ exchange/meta.go
@@ -96,6 +96,6 @@ func (e *ExchangeManager) initExchangeNames() {
 	supportList = append(supportList, HITBTC)
 	supportList = append(supportList, DRAGONEX)
 	supportList = append(supportList, BIGONE)
-	supportList = append(supportList, GATEIO)
 	supportList = append(supportList, BITFINEX)
+	supportList = append(supportList, GATEIO)
 }
