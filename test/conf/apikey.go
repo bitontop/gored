286deaf80a09b8ba2a2cce2760c5d3c9b0270e66
Mike Bao <by.bao@hotmail.com>
2019-05-30 14:04:44 -0700

    fix conflict Mike

--- test/conf/apikey.go
+++ test/conf/apikey.go
@@ -181,6 +181,11 @@ func Exchange(name exchange.ExchangeName, config *exchange.Config) {
 		config.Passphrase = ""
 		break
 
+	case exchange.BIKI:
+		config.API_KEY = ""
+		config.API_SECRET = ""
+		break
+
 	case exchange.BITATM:
 		config.API_KEY = ""
 		config.API_SECRET = ""
