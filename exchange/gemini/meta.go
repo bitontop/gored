22b923007169a79ef74dbc1a01a250f71b339175
tony0408 <604260645@qq.com>
2019-05-31 13:13:50 -0700

    push gemini pair data

---
+++ exchange/gemini/meta.go
@@ -0,0 +1,18 @@
+package gemini
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+const (
+	DEFAULT_ID           = 37
+	DEFAULT_TAKER_FEE    = 0.0035
+	DEFAULT_MAKER_FEE    = 0.001
+	DEFAULT_LOT_SIZE     = 0.00000001
+	DEFAULT_PRICE_FILTER = 0.00000001
+	DEFAULT_TXFEE        = 0.005
+	DEFAULT_WITHDRAW     = true
+	DEFAULT_DEPOSIT      = true
+	DEFAULT_CONFIRMATION = 2
+	DEFAULT_LISTED       = true
+)
---
+++ test/gemini_test.go
@@ -0,0 +1,69 @@
+package test
+
+import (
+	"log"
+	"testing"
+
+	"../exchange/gemini"
+	"./conf"
+	"github.com/bitontop/gored/coin"
+	"github.com/bitontop/gored/exchange"
+	"github.com/bitontop/gored/pair"
+	// "github.com/bitontop/gored/exchange/gemini"
+	// "github.com/bitontop/gored/test/conf"
+)
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/********************Public API********************/
+
+func Test_Gemini(t *testing.T) {
+	e := InitGemini()
+
+	pair := pair.GetPairByKey("BTC|ETH")
+
+	Test_Coins(e)
+	Test_Pairs(e)
+	Test_Pair(e, pair)
+	// Test_Orderbook(e, pair)
+	Test_ConstraintFetch(e, pair)
+	Test_Constraint(e, pair)
+
+	// Test_Balance(e, pair)
+	// Test_Trading(e, pair, 0.00000001, 100)
+	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
+}
+
+// test url and signature
+func Test_GeminiOrder(t *testing.T) {
+	e := InitGemini()
+
+	var order *exchange.Order
+	err := e.OrderStatus(order)
+	if err == nil {
+		log.Printf("%s Order Status: %v", e.GetName(), order)
+	} else {
+		log.Printf("%s Order Status Err: %s", e.GetName(), err)
+	}
+}
+
+func InitGemini() exchange.Exchange {
+	coin.Init()
+	pair.Init()
+
+	config := &exchange.Config{}
+	config.Source = exchange.EXCHANGE_API
+	// config.Source = exchange.JSON_FILE
+	// config.SourceURI = "https://raw.githubusercontent.com/bitontop/gored/master/data"
+	// utils.GetCommonDataFromJSON(config.SourceURI)
+
+	conf.Exchange(exchange.GEMINI, config)
+
+	ex := gemini.CreateGemini(config)
+	log.Printf("Initial [ %v ] ", ex.GetName())
+
+	config = nil
+	return ex
+}
