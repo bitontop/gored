9dddf67ee2cfbf054e02c2e824f49134333fd2ab
tony0408 <604260645@qq.com>
2019-06-07 13:12:12 -0700

    minor

--- exchange/model.go
+++ exchange/model.go
@@ -59,7 +59,7 @@ type ConstrainFetchMethod struct {
 	PublicAPI   bool
 	PrivateAPI  bool
 	HealthAPI   bool // get exchange health status from exchange's API directly
-	hasWithdraw bool
+	HasWithdraw bool
 
 	Fee          bool // true only when get Fee from API directly
 	LotSize      bool
