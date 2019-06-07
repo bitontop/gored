ea2064952894fb35e00ecbf7a35bf8d2a7efda54
tony0408 <604260645@qq.com>
2019-06-07 11:03:26 -0700

    bitbay init, implement later

---
+++ exchange/bitbay/model.go
@@ -0,0 +1,88 @@
+package bitbay
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import (
+	"encoding/json"
+
+	"github.com/bitontop/gored/exchange"
+)
+
+type JsonResponse struct {
+	Success bool            `json:"success"`
+	Message string          `json:"message"`
+	Result  json.RawMessage `json:"result"`
+}
+
+type AccountBalances []struct {
+	Currency      string  `json:"Currency"`
+	Balance       float64 `json:"Balance"`
+	Available     float64 `json:"Available"`
+	Pending       float64 `json:"Pending"`
+	CryptoAddress string  `json:"CryptoAddress"`
+	Requested     bool    `json:"Requested"`
+	Uuid          string  `json:"Uuid"`
+}
+
+type Uuid struct {
+	Id string `json:"uuid"`
+}
+
+type PlaceOrder struct {
+	AccountId                  string
+	OrderUuid                  string `json:"OrderUuid"`
+	Exchange                   string `json:"Exchange"`
+	Type                       string
+	Quantity                   float64 `json:"Quantity"`
+	QuantityRemaining          float64 `json:"QuantityRemaining"`
+	Limit                      float64 `json:"Limit"`
+	Reserved                   float64
+	ReserveRemaining           float64
+	CommissionReserved         float64
+	CommissionReserveRemaining float64
+	CommissionPaid             float64
+	Price                      float64 `json:"Price"`
+	PricePerUnit               float64 `json:"PricePerUnit"`
+	Opened                     string
+	Closed                     string
+	IsOpen                     bool
+	Sentinel                   string
+	CancelInitiated            bool
+	ImmediateOrCancel          bool
+	IsConditional              bool
+	Condition                  string
+	ConditionTarget            string
+}
+
+type PairsData []struct {
+	MarketCurrency     string      `json:"MarketCurrency"`
+	BaseCurrency       string      `json:"BaseCurrency"`
+	MarketCurrencyLong string      `json:"MarketCurrencyLong"`
+	BaseCurrencyLong   string      `json:"BaseCurrencyLong"`
+	MinTradeSize       float64     `json:"MinTradeSize"`
+	MarketName         string      `json:"MarketName"`
+	IsActive           bool        `json:"IsActive"`
+	Created            string      `json:"Created"`
+	Notice             interface{} `json:"Notice"`
+	IsSponsored        interface{} `json:"IsSponsored"`
+	LogoURL            string      `json:"LogoUrl"`
+}
+
+type CoinsData []struct {
+	Currency        string      `json:"Currency"`
+	CurrencyLong    string      `json:"CurrencyLong"`
+	MinConfirmation int         `json:"MinConfirmation"`
+	TxFee           float64     `json:"TxFee"`
+	IsActive        bool        `json:"IsActive"`
+	IsRestricted    bool        `json:"IsRestricted"`
+	CoinType        string      `json:"CoinType"`
+	BaseAddress     string      `json:"BaseAddress"`
+	Notice          interface{} `json:"Notice"`
+}
+
+type OrderBook struct {
+	Buy  []exchange.Order `json:"buy"`
+	Sell []exchange.Order `json:"sell"`
+}
---
+++ test/bitbay_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/bitontop/gored/coin"
+	"github.com/bitontop/gored/exchange"
+	"github.com/bitontop/gored/pair"
+
+	// "github.com/bitontop/gored/exchange/bitbay"
+	// "github.com/bitontop/gored/test/conf"
+	"../exchange/bitbay"
+	"./conf"
+)
+
+// Copyright (c) 2015-2019 Bitontop Technologies Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/********************Public API********************/
+
+func Test_Bitbay(t *testing.T) {
+	e := InitBitbay()
+
+	pair := pair.GetPairByKey("BTC|ETH")
+
+	Test_Coins(e)
+	Test_Pairs(e)
+	Test_Pair(e, pair)
+	// Test_Orderbook(e, pair)
+	Test_ConstraintFetch(e, pair)
+	Test_Constraint(e, pair)
+
+	// Test_Balance(e, pair)
+	// Test_Trading(e, pair, 0.00000001, 100)
+	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
+}
+
+// test url and signature
+func Test_BitbayOrder(t *testing.T) {
+	e := InitBitbay()
+
+	var order *exchange.Order
+	err := e.OrderStatus(order)
+	if err == nil {
+		log.Printf("%s Order Status: %v", e.GetName(), order)
+	} else {
+		log.Printf("%s Order Status Err: %s", e.GetName(), err)
+	}
+}
+
+func InitBitbay() exchange.Exchange {
+	coin.Init()
+	pair.Init()
+
+	config := &exchange.Config{}
+	config.Source = exchange.EXCHANGE_API
+	// config.Source = exchange.JSON_FILE
+	// config.SourceURI = "https://raw.githubusercontent.com/bitontop/gored/master/data"
+	// utils.GetCommonDataFromJSON(config.SourceURI)
+
+	conf.Exchange(exchange.BITBAY, config)
+
+	ex := bitbay.CreateBitbay(config)
+	log.Printf("Initial [ %v ] ", ex.GetName())
+
+	config = nil
+	return ex
+}
