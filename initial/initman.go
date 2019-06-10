fb5a9e9f8bac6b8e19c9a4bbba72cf0524626314
tony0408 <604260645@qq.com>
2019-06-10 15:34:24 -0700

    dont support gemini for now

--- initial/initman.go
+++ initial/initman.go
@@ -25,7 +25,6 @@ import (
 	"github.com/bitontop/gored/exchange/dcoin"
 	"github.com/bitontop/gored/exchange/dragonex"
 	"github.com/bitontop/gored/exchange/gateio"
-	"github.com/bitontop/gored/exchange/gemini"
 	"github.com/bitontop/gored/exchange/hitbtc"
 	"github.com/bitontop/gored/exchange/huobi"
 	"github.com/bitontop/gored/exchange/huobidm"
@@ -325,13 +324,6 @@ func (e *InitManager) Init(config *exchange.Config) exchange.Exchange {
 
 	case exchange.COINTIGER:
 		ex := cointiger.CreateCointiger(config)
-		if ex != nil {
-			e.exMan.Add(ex)
-		}
-		return ex
-
-	case exchange.GEMINI:
-		ex := gemini.CreateGemini(config)
 		if ex != nil {
 			e.exMan.Add(ex)
 		}
