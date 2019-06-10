1a5122cbe21860fe7a1f2e25612db346aa07410e
Mike Bao <by.bao@hotmail.com>
2019-06-10 14:48:03 -0700

    huobidm public

--- test/conf/apikey.go
+++ test/conf/apikey.go
@@ -205,5 +205,11 @@ func Exchange(name exchange.ExchangeName, config *exchange.Config) {
 		config.API_KEY = ""
 		config.API_SECRET = ""
 		break
+
+	case exchange.HUOBIDM:
+		config.API_KEY = ""
+		config.API_SECRET = ""
+		break
+
 	}
 }
