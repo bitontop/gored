3f26e356cea9cf4cad1d43902c97aa376dc9baad
Mike Bao <by.bao@hotmail.com>
2019-06-12 10:46:46 -0700

    huobidm ID change

--- exchange/huobidm/meta.go
+++ exchange/huobidm/meta.go
@@ -5,12 +5,12 @@ package huobidm
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 const (
-	DEFAULT_ID           = 99
+	DEFAULT_ID           = 42
 	DEFAULT_TAKER_FEE    = 0.0003
 	DEFAULT_MAKER_FEE    = 0.0002
 	DEFAULT_LOT_SIZE     = 1
 	DEFAULT_PRICE_FILTER = 0.01 //PRICE FILTER
-	DEFAULT_TXFEE        = 0
+	DEFAULT_TXFEE        = 0.05
 	DEFAULT_WITHDRAW     = false
 	DEFAULT_DEPOSIT      = false
 	DEFAULT_CONFIRMATION = 2
