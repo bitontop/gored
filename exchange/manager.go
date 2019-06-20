682d88d736bd4897a74eb8c8c1dc5c423520be6a
Your Name <temple@9cat.net>
2019-06-20 11:39:22 -0700

    exchange getstr upper case fix

--- exchange/manager.go
+++ exchange/manager.go
@@ -145,7 +145,7 @@ func (e *ExchangeManager) GetById(i int) Exchange {
 }
 
 func (e *ExchangeManager) GetStr(name string) Exchange {
-	name = strings.TrimSpace(name)
+	name = strings.ToUpper(strings.TrimSpace(name))
 	for _, v := range e.GetSupportExchanges() {
 		if string(v) == name {
 			return e.Get(v)
