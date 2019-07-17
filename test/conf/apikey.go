ec7bc6e7205d5678c964bd7076096252bc121142
tony0408 <604260645@qq.com>
2019-07-17 10:25:00 -0700

    apikey method

--- test/conf/apikey.go
+++ test/conf/apikey.go
@@ -256,5 +256,10 @@ func Exchange(name exchange.ExchangeName, config *exchange.Config) {
 		config.API_KEY = ""
 		config.API_SECRET = ""
 		break
+
+	case exchange.ABCC:
+		config.API_KEY = ""
+		config.API_SECRET = ""
+		break
 	}
 }
