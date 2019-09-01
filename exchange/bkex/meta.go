7fd39977a27bd486d86b1e9976c7f4369fd0a697
kakuilan <kakuilan@163.com>
2019-09-01 22:04:12 +0800

    fix bkex default value

--- exchange/bkex/meta.go
+++ exchange/bkex/meta.go
@@ -5,9 +5,9 @@ package bkex
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 const (
-	DEFAULT_ID           = 53
-	DEFAULT_TAKER_FEE    = 0.0025
-	DEFAULT_MAKER_FEE    = 0.0025
+	DEFAULT_ID           = 0
+	DEFAULT_TAKER_FEE    = 0.002
+	DEFAULT_MAKER_FEE    = 0.001
 	DEFAULT_LOT_SIZE     = 0.00000001
 	DEFAULT_PRICE_FILTER = 0.00000001
 
