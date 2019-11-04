44a2daedd94ed0bb458a9e31b3354e8ce7accc6c
Your Name <temple@9cat.net>
2019-11-04 08:26:16 -0800

    stat test case  optimized.

---
+++ test/stat/model.go
@@ -0,0 +1,12 @@
+// Copyright 2018 The Bitontop Tech Devlopment. All rights reserved.
+// license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"github.com/bitontop/gored/exchange"
+)
+
+type InitHandler struct {
+	ExMan *exchange.ExchangeManager
+}
--- test/stat/withdraw_test.go
+++ test/stat/withdraw_test.go
@@ -5,45 +5,39 @@ package test
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import (
-	"fmt"
 	"log"
-	"sync"
 	"testing"
 
-	"github.com/bitontop/gored/coin"
-	"github.com/bitontop/gored/exchange"
 	"github.com/bitontop/gored/pair"
-	"github.com/bitontop/gored/test/conf"
-
-	initial "github.com/bitontop/gored/initial"
-	utils "github.com/bitontop/gored/utils"
 	// "github.com/davecgh/go-spew/spew"
 )
 
 func Test_WithdrawEnableExchanges(t *testing.T) {
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
-	coin.Init()
-	pair.Init()
+	// coin.Init()
+	// pair.Init()
 
-	utils.GetCommonDataFromJSON("https://raw.githubusercontent.com/bitontop/gored/master/data") //e.config.SourceURI)
+	// utils.GetCommonDataFromJSON("https://raw.githubusercontent.com/bitontop/gored/master/data") //e.config.SourceURI)
 
-	exman := exchange.CreateExchangeManager()
-	exchanges := exman.GetSupportExchanges()
-	initMan := initial.CreateInitManager()
+	// exman := exchange.CreateExchangeManager()
+	// exchanges := exman.GetSupportExchanges()
+	// initMan := initial.CreateInitManager()
 
-	for _, ex := range exchanges {
-		InitEx(&wg, ex, initMan)
-	}
+	// for _, ex := range exchanges {
+	// 	InitEx(&wg, ex, initMan)
+	// }
 
-	wg.Wait()
+	// wg.Wait()
 
-	INITEX := fmt.Sprintf("Init DONE !")
-	log.Printf("%s %s %s ", COLOR_B_PINK, INITEX, COLOR_NO)
+	// INITEX := fmt.Sprintf("Init DONE !")
+	// log.Printf("%s %s %s ", COLOR_B_PINK, INITEX, COLOR_NO)
+
+	init := Init()
 
 	p := pair.GetPairByKey("BTC|ETH")
 
-	for _, ex := range exman.GetExchanges() {
+	for _, ex := range init.ExMan.GetExchanges() {
 		status := ex.GetConstraintFetchMethod(p)
 		// spew.Dump(status)
 
@@ -55,51 +49,4 @@ func Test_WithdrawEnableExchanges(t *testing.T) {
 
 	}
 
-}
-
-func InitEx(wg *sync.WaitGroup, name exchange.ExchangeName, initMan *initial.InitManager) {
-
-	wg.Add(1)
-
-	config := &exchange.Config{}
-	config.ExName = name
-	config.Source = exchange.JSON_FILE
-	config.SourceURI = "https://raw.githubusercontent.com/bitontop/gored/master/data"
-
-	conf.Exchange(name, config)
-
-	go func() {
-
-		ex := initMan.Init(config)
-		config = nil
-
-		if ex != nil {
-			log.Printf("%s Initial [ %12s ]  Coin:%d Pair:%d  %s", COLOR_B_PINK,
-				name,
-				len(ex.GetCoins()), len(ex.GetPairs()),
-				COLOR_NO)
-		} else {
-
-			coinQ := 0
-			if ex != nil {
-				coinQ = len(ex.GetCoins())
-			}
-			pairQ := 0
-			if ex != nil {
-				pairQ = len(ex.GetPairs())
-			}
-
-			log.Printf("%s FAIL to Initial [ %v ]  Coin:%d Pair:%d  %s ", COLOR_RED,
-				name,
-				coinQ, pairQ,
-				COLOR_NO)
-
-		}
-
-		if wg != nil {
-			wg.Done()
-
-		}
-
-	}()
 }
