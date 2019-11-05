042ad64a0be551b438502f48529790cb9e5bb315
botchun <chunlee@bitontop.com>
2019-11-05 13:48:11 -0800

    update huobiDM API

--- exchange/huobidm/api.go
+++ exchange/huobidm/api.go
@@ -236,6 +236,10 @@ func (e *Huobidm) OrderBook(p *pair.Pair) (*exchange.Maker, error) {
 }
 
 /*************** Private API ***************/
+func (e *Huobidm) DoAccoutOperation(operation *exchange.AccountOperation) error {
+	return nil
+}
+
 func (e *Huobidm) UpdateAllBalances() {
 	if e.API_KEY == "" || e.API_SECRET == "" {
 		log.Printf("%s API Key or Secret Key are nil.", e.GetName())
