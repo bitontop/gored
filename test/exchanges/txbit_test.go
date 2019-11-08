7b83e8876cbd741eca3f3873ae2fc634a9e58613
tony <tony.bitontop@gmail.com>
2019-11-07 16:44:51 -0800

    txbit withdraw done in new interface

--- test/exchanges/txbit_test.go
+++ test/exchanges/txbit_test.go
@@ -25,12 +25,12 @@ func Test_Txbit(t *testing.T) {
 
 	pair := pair.GetPairByKey("BTC|ETH")
 
-	Test_Coins(e)
-	Test_Pairs(e)
-	Test_Pair(e, pair)
-	Test_Orderbook(e, pair)
-	Test_ConstraintFetch(e, pair)
-	Test_Constraint(e, pair)
+	// Test_Coins(e)
+	// Test_Pairs(e)
+	// Test_Pair(e, pair)
+	// Test_Orderbook(e, pair)
+	// Test_ConstraintFetch(e, pair)
+	// Test_Constraint(e, pair)
 
 	// Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.001, 0.033)
@@ -39,6 +39,21 @@ func Test_Txbit(t *testing.T) {
 	// Test_CancelOrder(e, pair, "b5d7d18c-61fb-479e-8ee6-b222ced93e56")
 	// Test_Withdraw(e, pair.Target, 0.1, "0xf252be0c7758094a37bf10a4cbf4dec0d69b7bcc")
 	// log.Println(e.GetTradingWebURL(pair))
+
+	// Test Withdraw
+	opWithdraw := &exchange.AccountOperation{
+		Type:            exchange.Withdraw,
+		Coin:            pair.Target,
+		WithdrawAmount:  "1",
+		WithdrawAddress: "addr",
+		DebugMode:       true,
+	}
+	err := e.DoAccoutOperation(opWithdraw)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	log.Printf("WithdrawID: %v, err: %v", opWithdraw.WithdrawID, opWithdraw.Error)
+
 }
 
 func InitTxbit() exchange.Exchange {
