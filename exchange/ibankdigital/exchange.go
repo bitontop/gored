2fcf0d46dcbe522c93d739d643cf899bac420ab2
tony <tony.bitontop@gmail.com>
2019-11-08 16:59:29 -0800

    ibankdigital withdraw not valid

--- exchange/ibankdigital/exchange.go
+++ exchange/ibankdigital/exchange.go
@@ -236,7 +236,7 @@ func (e *Ibankdigital) GetConstraintFetchMethod(pair *pair.Pair) *exchange.Const
 	constrainFetchMethod.PublicAPI = true
 	constrainFetchMethod.PrivateAPI = true
 	constrainFetchMethod.HealthAPI = true
-	constrainFetchMethod.HasWithdraw = true
+	constrainFetchMethod.HasWithdraw = false
 	constrainFetchMethod.Fee = false
 	constrainFetchMethod.LotSize = true
 	constrainFetchMethod.PriceFilter = true
