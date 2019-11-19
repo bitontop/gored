2754883c407b158064b9f2eb4fbd2b4b226f3bff
tony <tony.bitontop@gmail.com>
2019-11-19 11:30:24 -0800

    minor

--- test/exchanges/bitpie_test.go
+++ test/exchanges/bitpie_test.go
@@ -25,12 +25,12 @@ func Test_Bitpie(t *testing.T) {
 
 	pair := pair.GetPairByKey("BTC|ETH")
 
-	// Test_Coins(e)
-	// Test_Pairs(e)
-	// Test_Pair(e, pair)
-	// Test_Orderbook(e, pair)
-	// Test_ConstraintFetch(e, pair)
-	// Test_Constraint(e, pair)
+	Test_Coins(e)
+	Test_Pairs(e)
+	Test_Pair(e, pair)
+	Test_Orderbook(e, pair)
+	Test_ConstraintFetch(e, pair)
+	Test_Constraint(e, pair)
 
 	Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
@@ -41,7 +41,7 @@ func Test_Bitpie(t *testing.T) {
 	// Test_CheckBalance(e, pair.Target, exchange.AssetWallet)
 	// Test_CheckAllBalance(e, exchange.AssetWallet)
 	// Test_DoTransfer(e, pair.Target, "1", exchange.AssetWallet, exchange.SpotWallet)
-	Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
+	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 
 }
 
