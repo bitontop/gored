fb3afe46da5f072b8f6372f023882353e2ff5f7a
tony <tony.bitontop@gmail.com>
2020-01-13 15:11:57 -0800

    update coinex test cases

--- test/exchanges/coinex_test.go
+++ test/exchanges/coinex_test.go
@@ -34,14 +34,17 @@ func Test_Coinex(t *testing.T) {
 
 	Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
+	// Test_Trading_Sell(e, pair, 0.06, 100)
 	// Test_OrderStatus(e, pair, "1234567890")
+	// Test_CancelOrder(e, pair, "1234567890")
 	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
 	// log.Println(e.GetTradingWebURL(pair))
 
+	// New Interface
 	// Test_CheckBalance(e, pair.Target, exchange.AssetWallet)
 	// Test_CheckAllBalance(e, exchange.SpotWallet)
 	// Test_DoTransfer(e, pair.Target, "1", exchange.AssetWallet, exchange.SpotWallet)
-	Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
+	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 
 }
 
