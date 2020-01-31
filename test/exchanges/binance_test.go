a77fb05db7238d60a14dba23327cf2e9252392a0
tony <tony.bitontop@gmail.com>
2020-01-30 16:22:52 -0800

    minoir

--- test/exchanges/binance_test.go
+++ test/exchanges/binance_test.go
@@ -22,19 +22,19 @@ import (
 func Test_Binance(t *testing.T) {
 	e := InitBinance()
 
-	pair := pair.GetPairByKey("BTC|ETH")
+	pair := pair.GetPairByKey("BTC|TCT")
 
-	Test_Coins(e)
+	// Test_Coins(e)
 	Test_Pairs(e)
 	Test_Pair(e, pair)
-	Test_Orderbook(e, pair)
-	Test_ConstraintFetch(e, pair)
-	Test_Constraint(e, pair)
+	// Test_Orderbook(e, pair)
+	// Test_ConstraintFetch(e, pair)
+	// Test_Constraint(e, pair)
 
 	Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
 	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
-	Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
+	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 }
 
 func InitBinance() exchange.Exchange {
@@ -42,6 +42,10 @@ func InitBinance() exchange.Exchange {
 	pair.Init()
 	config := &exchange.Config{}
 	config.Source = exchange.EXCHANGE_API
+	// config.Source = exchange.JSON_FILE
+	// config.SourceURI = "https://raw.githubusercontent.com/bitontop/gored/master/data"
+	// utils.GetCommonDataFromJSON(config.SourceURI)
+
 	conf.Exchange(exchange.BINANCE, config)
 
 	ex := binance.CreateBinance(config)
