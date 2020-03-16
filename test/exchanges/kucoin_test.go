b85e07a62bfb9f7cc52d613e0f0c9a4490ec5892
tony <tony.bitontop@gmail.com>
2020-03-16 15:41:53 -0700

    minor

--- test/exchanges/kucoin_test.go
+++ test/exchanges/kucoin_test.go
@@ -32,14 +32,15 @@ func Test_Kucoin(t *testing.T) {
 	// Test_ConstraintFetch(e, pair)
 	// Test_Constraint(e, pair)
 
-	// Test_Balance(e, pair)
+	Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
+	// Test_Trading_Sell(e, pair, 0.06, 0.01)
 	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
 
 	Test_CheckBalance(e, pair.Target, exchange.AssetWallet)
-	Test_CheckAllBalance(e, exchange.AssetWallet)
-	Test_DoTransfer(e, pair.Target, "1", exchange.AssetWallet, exchange.SpotWallet)
-	Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
+	Test_CheckAllBalance(e, exchange.SpotWallet)
+	// Test_DoTransfer(e, pair.Target, "1", exchange.AssetWallet, exchange.SpotWallet)
+	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 }
 
 func Test_KUCOIN_TradeHistory(t *testing.T) {
