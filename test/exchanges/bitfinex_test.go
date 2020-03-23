6d67c7985a5434a7bee8e578436e191e6db3012b
tony0408 <604260645@qq.com>
2020-03-23 15:12:28 -0700

    minor

--- test/exchanges/bitfinex_test.go
+++ test/exchanges/bitfinex_test.go
@@ -22,10 +22,11 @@ func Test_Bitfinex(t *testing.T) {
 	// Test_Pairs(e)
 	Test_Pair(e, pair)
 	// Test_Orderbook(e, pair)
+	Test_NewOrderBook(e, pair)
 	// Test_ConstraintFetch(e, pair)
 	// Test_Constraint(e, pair)
 
-	Test_Balance(e, pair)
+	// Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.0001000123, 100)
 	// Test_Trading_Sell(e, pair, 0.06123456789, 0.02)
 	// Test_OrderStatus(e, pair, "40797839810") // 40797839810 an open order
@@ -34,5 +35,5 @@ func Test_Bitfinex(t *testing.T) {
 	// log.Printf("Url: %v", e.GetTradingWebURL(pair))
 	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 
-	Test_TradeHistory(e, pair)
+	// Test_TradeHistory(e, pair)
 }
--- test/exchanges/okex_test.go
+++ test/exchanges/okex_test.go
@@ -23,19 +23,20 @@ func Test_Okex(t *testing.T) {
 	// Test_Pairs(e)
 	Test_Pair(e, pair)
 	// Test_Orderbook(e, pair)
+	Test_NewOrderBook(e, pair)
 	// Test_ConstraintFetch(e, pair)
 	// Test_Constraint(e, pair)
 
 	// new interface methods
 	// Test_DoWithdraw(e, pair.Target, "1", "0x37E0Fc27C6cDB5035B2a3d0682B4E7C05A4e6C46", "tag")
 	// Test_DoTransfer(e, pair.Target, "10", exchange.AssetWallet, exchange.SpotWallet)
-	Test_CheckBalance(e, pair.Target, exchange.AssetWallet)
-	Test_CheckAllBalance(e, exchange.AssetWallet)
+	// Test_CheckBalance(e, pair.Target, exchange.AssetWallet)
+	// Test_CheckAllBalance(e, exchange.AssetWallet)
 
 	// okex.Socket(pair)
 	// Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
 	// Test_Withdraw(e, pair.Base, 1, "ADDRESS")
 
-	Test_TradeHistory(e, pair)
+	// Test_TradeHistory(e, pair)
 }
