8ea621324120d7c03d1432898f31d8f3e7551776
tony0408 <604260645@qq.com>
2020-06-24 15:23:10 -0700

    tickerPrice

--- test/exchanges/coinex_test.go
+++ test/exchanges/coinex_test.go
@@ -28,7 +28,7 @@ func Test_Coinex(t *testing.T) {
 	// Test_ConstraintFetch(e, pair)
 	// Test_Constraint(e, pair)
 
-	// Test_TickerPrice(e)
+	Test_TickerPrice(e)
 
 	Test_Balance(e, pair)
 	// Test_Trading(e, pair, 0.00000001, 100)
